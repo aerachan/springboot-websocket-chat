9ce0f674e1c1954124262d2a99ebf5474965038b
aerachan <almanzoryasmine@gmail.com>
2023-12-22 14:40:44 +0100

    Initial commit

---
+++ src/main/java/com/example/chatapi/websocketchatapi/WebsocketChatApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatapi.websocketchatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketChatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketChatApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/chatapi/websocketchatapi/chat/MessageType.java
@@ -0,0 +1,9 @@
+package com.example.chatapi.websocketchatapi.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+
+}
